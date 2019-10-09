897ee59fad9fd959780f5c2f2a2c01fe8b814c7f
Dani Roman <roman.martinez.daniel@gmail.com>
2019-10-09 21:17:43 +0200

    Actividad 1 y Actividad 2

---
+++ examen_html.php
@@ -0,0 +1,25 @@
+<h1>Examen Monica:</h1>
+
+<p> Nombre: <?php echo $_POST['name']; ?>
+			
+<p> Correo electrónico: <?php echo $_POST['email']; ?>
+
+<p> URL: <?php echo $_POST['url']; ?>
+
+<p>Fecha: <?php echo $_POST['date']; ?>
+
+<p>Tiempo: <?php echo $_POST['time']; ?>
+
+<p>Fecha y hora: <?php echo $_POST['datetime']; ?>
+
+<p>Mes: <?php echo $_POST['month']; ?>
+
+<p>Semana: <?php echo $_POST['week']; ?>
+
+<p>Número <?php echo $_POST['number']; ?>
+
+<p>Teléfono: <?php echo $_POST['phone']; ?>
+
+<p>Término de búsqueda: <?php echo $_POST['search']; ?>
+
+<p>Color favorito: <?php echo $_POST['favourite_color']; ?>
